8d8a89130ca1830a128c4a8a85fb0e6bb1eab5a0
muhammad galuh gumelar <mggart39@gmail.com>
2026-01-29 19:36:34 +0700

    revisi

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,34 +1,8 @@
-import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
-import { config } from "../config";
-
-export function authenticateToken(req: Request, res: Response, next: NextFunction) {
-  const authHeader = req.header("Authorization");
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ error: "Access Denied. No token provided." });
-  }
-
-  const token = authHeader.split(" ")[1];
-
-  try {
-    const verified = jwt.verify(token, config.jwtSecret);
-    (req as any).user = verified;
-    next();
-  } catch (err) {
-    res.status(403).json({ error: "Invalid Token", details: err });
-  }
-}
-
 // import { Request, Response, NextFunction } from "express";
 // import jwt from "jsonwebtoken";
 // import { config } from "../config";
-// import { CustomRequest } from "@/types";
 
-// export function authenticateToken(
-//   req: Request,
-//   res: Response,
-//   next: NextFunction,
-// ) {
+// export function authenticateToken(req: Request, res: Response, next: NextFunction) {
 //   const authHeader = req.header("Authorization");
 //   if (!authHeader || !authHeader.startsWith("Bearer ")) {
 //     return res.status(401).json({ error: "Access Denied. No token provided." });
@@ -37,10 +11,36 @@ export function authenticateToken(req: Request, res: Response, next: NextFunctio
 //   const token = authHeader.split(" ")[1];
 
 //   try {
-//     const verified = jwt.verify(token, config.jwtSecret) as any;
-//     (req as CustomRequest).user = verified.user;
+//     const verified = jwt.verify(token, config.jwtSecret);
+//     (req as any).user = verified;
 //     next();
 //   } catch (err) {
 //     res.status(403).json({ error: "Invalid Token", details: err });
 //   }
 // }
+
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { config } from "../config";
+import { CustomRequest } from "@/types";
+
+export function authenticateToken(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const authHeader = req.header("Authorization");
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Access Denied. No token provided." });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const verified = jwt.verify(token, config.jwtSecret) as any;
+    (req as CustomRequest).user = verified.user;
+    next();
+  } catch (err) {
+    res.status(403).json({ error: "Invalid Token", details: err });
+  }
+}
